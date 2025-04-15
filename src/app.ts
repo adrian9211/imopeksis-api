6e5de726e12ea6049aad97f6d138ee6b592b74a6
Adria Nykiel <adrian.nykiel@gmail.com>
2025-04-15 10:32:28 +0100

    CORS import

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import articlesRouter from './routes/articles.js'
 import path from 'path'
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 // Serve static audio
