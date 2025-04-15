017a50fcd4d24b9445b06bcc8ae94520d852c3b9
Adria Nykiel <adrian.nykiel@gmail.com>
2025-04-15 11:46:44 +0100

    article empty array fix v10

--- src/articles/index.ts
+++ src/articles/index.ts
@@ -1,5 +1,109 @@
+import { article as understanding_psychosocial_competencies_in_children_and_adolescents___the_key_to_upbringing } from "./understanding-psychosocial-competencies-in-children-and-adolescents---the-key-to-upbringing";
+import { article as quality_of_movement__a_practical_lesson_in_imopeksis_methodology_in_dance } from "./quality-of-movement--a-practical-lesson-in-imopeksis-methodology-in-dance";
+import { article as imopeksis__from_knowledge_to_value___shaping_social_competencies_and_child_development } from "./imopeksis--from-knowledge-to-value---shaping-social-competencies-and-child-development";
+import { article as child_development__between_nature_and_the_influence_of_adults } from "./child-development--between-nature-and-the-influence-of-adults";
+import { article as imopeksis_for_parents__social_competence___from_knowledge_to_values } from "./imopeksis-for-parents--social-competence---from-knowledge-to-values";
+import { article as imopeksis_clinic__training_summary___practical_conclusions_and_reflections } from "./imopeksis-clinic--training-summary---practical-conclusions-and-reflections";
+import { article as imopeksis__from_teaching_to_reflection___the_key_to_child_development } from "./imopeksis--from-teaching-to-reflection---the-key-to-child-development";
+import { article as practical_application_of_disc_behavioural_models_in_sport__team_building_and_individualised_approach } from "./practical-application-of-disc-behavioural-models-in-sport--team-building-and-individualised-approach";
+import { article as imopeksis__from_theory_to_practice___reflection__independence_and_understanding_the_child } from "./imopeksis--from-theory-to-practice---reflection--independence-and-understanding-the-child";
+import { article as imopeksis_for_parents__the_path_to_a_child_s_happiness_and_fulfillment } from "./imopeksis-for-parents--the-path-to-a-child-s-happiness-and-fulfillment";
+import { article as imopeksis_clinic__technical_periodization_and_psychopedagogy_in_basketball_training } from "./imopeksis-clinic--technical-periodization-and-psychopedagogy-in-basketball-training";
+import { article as quality_of_movement_in_practice__continuing_dance_lessons_using_the_imopeksis_method } from "./quality-of-movement-in-practice--continuing-dance-lessons-using-the-imopeksis-method";
 import { article as behavioural_models_in_sport_and_education__understanding_and_utilising_individual_differences } from "./behavioural-models-in-sport-and-education--understanding-and-utilising-individual-differences";
+import { article as imopeksis_clinic__behavioural_modules_and_psychopedagogy_in_basketball_training } from "./imopeksis-clinic--behavioural-modules-and-psychopedagogy-in-basketball-training";
+import { article as imopeksis_clinic__moving_without_the_ball___practical_training_for_basketball_coaches } from "./imopeksis-clinic--moving-without-the-ball---practical-training-for-basketball-coaches";
+import { article as imopeksis__education_through_understanding__not_through_coercion } from "./imopeksis--education-through-understanding--not-through-coercion";
+import { article as imopeksis_for_parents__communication__emotions__and_supporting_children_s_independence } from "./imopeksis-for-parents--communication--emotions--and-supporting-children-s-independence";
+import { article as imopeksis__shaping_social_competencies___from_knowledge_to_values } from "./imopeksis--shaping-social-competencies---from-knowledge-to-values";
+import { article as imopeksis__understanding_the_child__reflection_and_inner_growth } from "./imopeksis--understanding-the-child--reflection-and-inner-growth";
+import { article as quality_of_movement__a_practical_lesson_in_the_imopexis_methodology_in_dance } from "./quality-of-movement--a-practical-lesson-in-the-imopexis-methodology-in-dance";
+import { article as application_of_disc_behavioural_models_in_sport_and_education__practical_examples_and_conclusions } from "./application-of-disc-behavioural-models-in-sport-and-education--practical-examples-and-conclusions";
+import { article as imopexis_in_practice__independence__reflection_and_understanding_of_the_child } from "./imopexis-in-practice--independence--reflection-and-understanding-of-the-child";
+import { article as imopeksis_for_parents__understanding_and_developing_psychosocial_competencies } from "./imopeksis-for-parents--understanding-and-developing-psychosocial-competencies";
+import { article as imopeksis_for_parents__understanding_your_child_and_supporting_their_development } from "./imopeksis-for-parents--understanding-your-child-and-supporting-their-development";
+import { article as reflection_and_practice__summary_of_the_imopeksis_methodology_and_benefits_for_teachers } from "./reflection-and-practice--summary-of-the-imopeksis-methodology-and-benefits-for-teachers";
+import { article as from_knowledge_to_transcendence__shaping_social_competencies_in_the_spirit_of_imopeksis } from "./from-knowledge-to-transcendence--shaping-social-competencies-in-the-spirit-of-imopeksis";
+import { article as imopeksis_clinic__lectures_for_coaches___understanding_and_shaping_athletes } from "./imopeksis-clinic--lectures-for-coaches---understanding-and-shaping-athletes";
+import { article as imopeksis_for_parents__the_path_to_growth___from_knowledge_to_transcendence } from "./imopeksis-for-parents--the-path-to-growth---from-knowledge-to-transcendence";
+import { article as imopeksis_for_parents__from_knowledge_to_values___developing_psychosocial_competencies__part_2_ } from "./imopeksis-for-parents--from-knowledge-to-values---developing-psychosocial-competencies--part-2-";
+import { article as revolution_in_education__learning_from_children_with_the_imopeksis_methodology } from "./revolution-in-education--learning-from-children-with-the-imopeksis-methodology";
+import { article as imopeksis__understanding_behavioural_models_to_work_more_effectively_with_children } from "./imopeksis--understanding-behavioural-models-to-work-more-effectively-with-children";
+import { article as neurodidactics_in_practice__how_to_develop_a_child_s_potential_instead_of_suppressing_it } from "./neurodidactics-in-practice--how-to-develop-a-child-s-potential-instead-of-suppressing-it";
+import { article as the_philosophy_of_imopeksis__building_a_team_from_within } from "./the-philosophy-of-imopeksis--building-a-team-from-within";
+import { article as social_competencies_and_child_development__from_knowledge_to_values } from "./social-competencies-and-child-development--from-knowledge-to-values";
+import { article as imopexis_clinic__special_applications___continuation_and_development } from "./imopexis-clinic--special-applications---continuation-and-development";
+import { article as imopeksis_in_practice__from_theory_to_application___interaction__reflection_and_individualisation } from "./imopeksis-in-practice--from-theory-to-application---interaction--reflection-and-individualisation";
+import { article as technical_periodization_in_basketball__the_methodology_of_imopeksis_in_practice } from "./technical-periodization-in-basketball--the-methodology-of-imopeksis-in-practice";
+import { article as imopeksis_for_parents__from_knowledge_to_values___shaping_social_competencies } from "./imopeksis-for-parents--from-knowledge-to-values---shaping-social-competencies";
+import { article as imopeksis_for_parents__understanding_the_child__reflection_and_supporting_development } from "./imopeksis-for-parents--understanding-the-child--reflection-and-supporting-development";
+import { article as imopeksis__from_theory_to_practice___shaping_competencies_and_child_development } from "./imopeksis--from-theory-to-practice---shaping-competencies-and-child-development";
+import { article as imopeksis_clinic__special_applications___practical_training_for_basketball_coaches } from "./imopeksis-clinic--special-applications---practical-training-for-basketball-coaches";
+import { article as imopeksis_clinic__special_applications___practical_training_for_basketball_coaches__part_2_ } from "./imopeksis-clinic--special-applications---practical-training-for-basketball-coaches--part-2-";
+import { article as imopeksis_for_parents__independence__reflection_and_development_of_psychosocial_competencies } from "./imopeksis-for-parents--independence--reflection-and-development-of-psychosocial-competencies";
+import { article as the_philosophy_of_imopexis__building_a_team_from_within } from "./the-philosophy-of-imopexis--building-a-team-from-within";
+import { article as imopeksis_for_parents__understanding_psychosocial_competencies___practical_application } from "./imopeksis-for-parents--understanding-psychosocial-competencies---practical-application";
+import { article as from_robot_to_dance__the_application_of_the_imopeksis_methodology_in_practice } from "./from-robot-to-dance--the-application-of-the-imopeksis-methodology-in-practice";
+import { article as imopeksis__motivation__reflection_and_understanding_in_the_learning_process } from "./imopeksis--motivation--reflection-and-understanding-in-the-learning-process";
+import { article as imopeksis_clinic__training_summary___analysis_and_conclusions } from "./imopeksis-clinic--training-summary---analysis-and-conclusions";
+import { article as practical_dance_lesson_using_the_imopeksis_method__movement__emotions__and_independence } from "./practical-dance-lesson-using-the-imopeksis-method--movement--emotions--and-independence";
+import { article as imopeksis_for_parents__developing_psychosocial_competencies___from_knowledge_to_values__part_2 } from "./imopeksis-for-parents--developing-psychosocial-competencies---from-knowledge-to-values--part-2";
+import { article as from_neurodidactics_to_practice__revolution_in_physical_education_with_the_imopeksis_method } from "./from-neurodidactics-to-practice--revolution-in-physical-education-with-the-imopeksis-method";
+import { article as child___a_silenced_genius__how_to_support_development_instead_of_hindering_it } from "./child---a-silenced-genius--how-to-support-development-instead-of-hindering-it";
 
 const articles = {
-    [behavioural_models_in_sport_and_education__understanding_and_utilising_individual_differences.slug]: behavioural_models_in_sport_and_education__understanding_and_utilising_individual_differences
-}
+    [understanding_psychosocial_competencies_in_children_and_adolescents___the_key_to_upbringing.slug]: understanding_psychosocial_competencies_in_children_and_adolescents___the_key_to_upbringing,
+    [quality_of_movement__a_practical_lesson_in_imopeksis_methodology_in_dance.slug]: quality_of_movement__a_practical_lesson_in_imopeksis_methodology_in_dance,
+    [imopeksis__from_knowledge_to_value___shaping_social_competencies_and_child_development.slug]: imopeksis__from_knowledge_to_value___shaping_social_competencies_and_child_development,
+    [child_development__between_nature_and_the_influence_of_adults.slug]: child_development__between_nature_and_the_influence_of_adults,
+    [imopeksis_for_parents__social_competence___from_knowledge_to_values.slug]: imopeksis_for_parents__social_competence___from_knowledge_to_values,
+    [imopeksis_clinic__training_summary___practical_conclusions_and_reflections.slug]: imopeksis_clinic__training_summary___practical_conclusions_and_reflections,
+    [imopeksis__from_teaching_to_reflection___the_key_to_child_development.slug]: imopeksis__from_teaching_to_reflection___the_key_to_child_development,
+    [practical_application_of_disc_behavioural_models_in_sport__team_building_and_individualised_approach.slug]: practical_application_of_disc_behavioural_models_in_sport__team_building_and_individualised_approach,
+    [imopeksis__from_theory_to_practice___reflection__independence_and_understanding_the_child.slug]: imopeksis__from_theory_to_practice___reflection__independence_and_understanding_the_child,
+    [imopeksis_for_parents__the_path_to_a_child_s_happiness_and_fulfillment.slug]: imopeksis_for_parents__the_path_to_a_child_s_happiness_and_fulfillment,
+    [imopeksis_clinic__technical_periodization_and_psychopedagogy_in_basketball_training.slug]: imopeksis_clinic__technical_periodization_and_psychopedagogy_in_basketball_training,
+    [quality_of_movement_in_practice__continuing_dance_lessons_using_the_imopeksis_method.slug]: quality_of_movement_in_practice__continuing_dance_lessons_using_the_imopeksis_method,
+    [behavioural_models_in_sport_and_education__understanding_and_utilising_individual_differences.slug]: behavioural_models_in_sport_and_education__understanding_and_utilising_individual_differences,
+    [imopeksis_clinic__behavioural_modules_and_psychopedagogy_in_basketball_training.slug]: imopeksis_clinic__behavioural_modules_and_psychopedagogy_in_basketball_training,
+    [imopeksis_clinic__moving_without_the_ball___practical_training_for_basketball_coaches.slug]: imopeksis_clinic__moving_without_the_ball___practical_training_for_basketball_coaches,
+    [imopeksis__education_through_understanding__not_through_coercion.slug]: imopeksis__education_through_understanding__not_through_coercion,
+    [imopeksis_for_parents__communication__emotions__and_supporting_children_s_independence.slug]: imopeksis_for_parents__communication__emotions__and_supporting_children_s_independence,
+    [imopeksis__shaping_social_competencies___from_knowledge_to_values.slug]: imopeksis__shaping_social_competencies___from_knowledge_to_values,
+    [imopeksis__understanding_the_child__reflection_and_inner_growth.slug]: imopeksis__understanding_the_child__reflection_and_inner_growth,
+    [quality_of_movement__a_practical_lesson_in_the_imopexis_methodology_in_dance.slug]: quality_of_movement__a_practical_lesson_in_the_imopexis_methodology_in_dance,
+    [application_of_disc_behavioural_models_in_sport_and_education__practical_examples_and_conclusions.slug]: application_of_disc_behavioural_models_in_sport_and_education__practical_examples_and_conclusions,
+    [imopexis_in_practice__independence__reflection_and_understanding_of_the_child.slug]: imopexis_in_practice__independence__reflection_and_understanding_of_the_child,
+    [imopeksis_for_parents__understanding_and_developing_psychosocial_competencies.slug]: imopeksis_for_parents__understanding_and_developing_psychosocial_competencies,
+    [imopeksis_for_parents__understanding_your_child_and_supporting_their_development.slug]: imopeksis_for_parents__understanding_your_child_and_supporting_their_development,
+    [reflection_and_practice__summary_of_the_imopeksis_methodology_and_benefits_for_teachers.slug]: reflection_and_practice__summary_of_the_imopeksis_methodology_and_benefits_for_teachers,
+    [from_knowledge_to_transcendence__shaping_social_competencies_in_the_spirit_of_imopeksis.slug]: from_knowledge_to_transcendence__shaping_social_competencies_in_the_spirit_of_imopeksis,
+    [imopeksis_clinic__lectures_for_coaches___understanding_and_shaping_athletes.slug]: imopeksis_clinic__lectures_for_coaches___understanding_and_shaping_athletes,
+    [imopeksis_for_parents__the_path_to_growth___from_knowledge_to_transcendence.slug]: imopeksis_for_parents__the_path_to_growth___from_knowledge_to_transcendence,
+    [imopeksis_for_parents__from_knowledge_to_values___developing_psychosocial_competencies__part_2_.slug]: imopeksis_for_parents__from_knowledge_to_values___developing_psychosocial_competencies__part_2_,
+    [revolution_in_education__learning_from_children_with_the_imopeksis_methodology.slug]: revolution_in_education__learning_from_children_with_the_imopeksis_methodology,
+    [imopeksis__understanding_behavioural_models_to_work_more_effectively_with_children.slug]: imopeksis__understanding_behavioural_models_to_work_more_effectively_with_children,
+    [neurodidactics_in_practice__how_to_develop_a_child_s_potential_instead_of_suppressing_it.slug]: neurodidactics_in_practice__how_to_develop_a_child_s_potential_instead_of_suppressing_it,
+    [the_philosophy_of_imopeksis__building_a_team_from_within.slug]:  the_philosophy_of_imopeksis__building_a_team_from_within,
+    [social_competencies_and_child_development__from_knowledge_to_values.slug]: social_competencies_and_child_development__from_knowledge_to_values,
+    [imopexis_clinic__special_applications___continuation_and_development.slug]: imopexis_clinic__special_applications___continuation_and_development,
+    [imopeksis_in_practice__from_theory_to_application___interaction__reflection_and_individualisation.slug]: imopeksis_in_practice__from_theory_to_application___interaction__reflection_and_individualisation,
+    [technical_periodization_in_basketball__the_methodology_of_imopeksis_in_practice.slug]: technical_periodization_in_basketball__the_methodology_of_imopeksis_in_practice,
+    [imopeksis_for_parents__from_knowledge_to_values___shaping_social_competencies.slug]: imopeksis_for_parents__from_knowledge_to_values___shaping_social_competencies,
+    [imopeksis_for_parents__understanding_the_child__reflection_and_supporting_development.slug]: imopeksis_for_parents__understanding_the_child__reflection_and_supporting_development,
+    [imopeksis__from_theory_to_practice___shaping_competencies_and_child_development.slug]: imopeksis__from_theory_to_practice___shaping_competencies_and_child_development,
+    [imopeksis_clinic__special_applications___practical_training_for_basketball_coaches.slug]: imopeksis_clinic__special_applications___practical_training_for_basketball_coaches,
+    [imopeksis_clinic__special_applications___practical_training_for_basketball_coaches__part_2_.slug]: imopeksis_clinic__special_applications___practical_training_for_basketball_coaches__part_2_,
+    [imopeksis_for_parents__independence__reflection_and_development_of_psychosocial_competencies.slug]: imopeksis_for_parents__independence__reflection_and_development_of_psychosocial_competencies,
+    [the_philosophy_of_imopexis__building_a_team_from_within.slug]: the_philosophy_of_imopexis__building_a_team_from_within,
+    [imopeksis_for_parents__understanding_psychosocial_competencies___practical_application.slug]: imopeksis_for_parents__understanding_psychosocial_competencies___practical_application,
+    [from_robot_to_dance__the_application_of_the_imopeksis_methodology_in_practice.slug]: from_robot_to_dance__the_application_of_the_imopeksis_methodology_in_practice,
+    [imopeksis__motivation__reflection_and_understanding_in_the_learning_process.slug]: imopeksis__motivation__reflection_and_understanding_in_the_learning_process,
+    [imopeksis_clinic__training_summary___analysis_and_conclusions.slug]: imopeksis_clinic__training_summary___analysis_and_conclusions,
+    [practical_dance_lesson_using_the_imopeksis_method__movement__emotions__and_independence.slug]: practical_dance_lesson_using_the_imopeksis_method__movement__emotions__and_independence,
+    [imopeksis_for_parents__developing_psychosocial_competencies___from_knowledge_to_values__part_2.slug]: imopeksis_for_parents__developing_psychosocial_competencies___from_knowledge_to_values__part_2,
+    [from_neurodidactics_to_practice__revolution_in_physical_education_with_the_imopeksis_method.slug]: from_neurodidactics_to_practice__revolution_in_physical_education_with_the_imopeksis_method,
+    [child___a_silenced_genius__how_to_support_development_instead_of_hindering_it.slug]: child___a_silenced_genius__how_to_support_development_instead_of_hindering_it
+};
+
+export default articles;
